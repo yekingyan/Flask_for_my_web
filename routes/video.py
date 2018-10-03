6e266590e926048be0a3e58d1827b5613a89bb23
yekingyan <529616@gmail.com>
2018-10-04 02:13:46 +0800

    一些较小的修改

--- routes/video.py
+++ routes/video.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint,
     render_template,
 )
-
+from models.user import current_user_name
 
 # 创建蓝图,蓝图名为main
 main = Blueprint('video', __name__)
@@ -10,4 +10,5 @@ main = Blueprint('video', __name__)
 
 @main.route('/')
 def index():
-    return render_template('video.html')
+    username = current_user_name()
+    return render_template('video.html', title='视频', username=username)
